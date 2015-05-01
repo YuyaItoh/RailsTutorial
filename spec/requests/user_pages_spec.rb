a454706531bcb711c20a2a3444c2fb0b3db85405
YuyaItoh <y-itoh@ai.soc.i.kyoto-u.ac.jp>
2015-05-02 01:23:54 +0900

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,47 @@ describe "UserPages" do
   let(:title) {"Ruby on Rails Tutorial Sample App | #{topic}"}
   subject { page }
 
+  # signupビューテスト
   describe "signup page" do
     before { visit signup_path }
     let(:topic) { 'Sign up' }
 
     it { should have_content(topic) }
     it { should have_title(title) }
+  end
+
+  # showビューテスト
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  # signup処理テスト
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    # 不正入力のテスト
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect {click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",          with: "Example_User"
+        fill_in "Email",         with: "user@example.com"
+        fill_in "Password",      with: "foobar"
+        fill_in "Confirmation",  with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
